0175ec5bb9d08ec57861a84e20e6484b24d1db8d
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-03 00:54:30 +0530

    DE

--- 01DE.py
+++ 01DE.py
@@ -1,2 +1,48 @@
 import pandas as pd
+import glob                         # this module helps in selecting files 
+import pandas as pd                 # this module helps in processing CSV files
+import xml.etree.ElementTree as ET  # this module helps in processing XML files.
+from datetime import datetime
+
+tmpfile    = "temp.tmp"               # file used to store all extracted data
+logfile    = "logfile.txt"            # all event logs will be stored in this file
+targetfile = "transformed_data.csv"   # file where transformed data is stored
+
+def extract_from_csv(file_to_process):
+    dataframe = pd.read_csv(file_to_process)
+    return dataframe
+
+def extract_from_json(file_to_process):
+    dataframe = pd.read_json(file_to_process,lines=True)
+    return dataframe
+
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
+    tree = ET.parse(file_to_process)
+    root = tree.getroot()
+    for person in root:
+        name = person.find("name").text
+        height = float(person.find("height").text)
+        weight = float(person.find("weight").text)
+        dataframe = dataframe.append({"name":name, "height":height, "weight":weight}, ignore_index=True)
+    return dataframe
+
+def extract():
+    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data
+    
+    #process all csv files
+    for csvfile in glob.glob("./source/*.csv"):
+        extracted_data = extracted_data.append(extract_from_csv(csvfile), ignore_index=True)
+        
+    #process all json files
+    for jsonfile in glob.glob("./source/*.json"):
+        extracted_data = extracted_data.append(extract_from_json(jsonfile), ignore_index=True)
+    
+    #process all xml files
+    for xmlfile in glob.glob("./source/*.xml"):
+        extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)
+        
+    return extracted_data
+
+
 
