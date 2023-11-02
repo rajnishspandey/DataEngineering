7dc74008cd25e4d8373437fd625182fc1238eeb8
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-03 01:08:26 +0530

    DE

--- 01DE.py
+++ 01DE.py
@@ -4,18 +4,23 @@ import pandas as pd                 # this module helps in processing CSV files
 import xml.etree.ElementTree as ET  # this module helps in processing XML files.
 from datetime import datetime
 
+#set Path
 tmpfile    = "temp.tmp"               # file used to store all extracted data
 logfile    = "logfile.txt"            # all event logs will be stored in this file
 targetfile = "transformed_data.csv"   # file where transformed data is stored
 
+#Extract
+#CSV Extract Function
 def extract_from_csv(file_to_process):
     dataframe = pd.read_csv(file_to_process)
     return dataframe
 
+#JSON Extract Function
 def extract_from_json(file_to_process):
     dataframe = pd.read_json(file_to_process,lines=True)
     return dataframe
 
+#XML Extract Function
 def extract_from_xml(file_to_process):
     dataframe = pd.DataFrame(columns=["name", "height", "weight"])
     tree = ET.parse(file_to_process)
@@ -27,6 +32,7 @@ def extract_from_xml(file_to_process):
         dataframe = dataframe.append({"name":name, "height":height, "weight":weight}, ignore_index=True)
     return dataframe
 
+#Extract Function
 def extract():
     extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data
     
@@ -44,5 +50,47 @@ def extract():
         
     return extracted_data
 
+#Transform
+def transform(data):
+        #Convert height which is in inches to millimeter
+        #Convert the datatype of the column into float
+        #data.height = data.height.astype(float)
+        #Convert inches to meters and round off to two decimals(one inch is 0.0254 meters)
+        data['height'] = round(data.height * 0.0254,2)
+        
+        #Convert weight which is in pounds to kilograms
+        #Convert the datatype of the column into float
+        #data.weight = data.weight.astype(float)
+        #Convert pounds to kilograms and round off to two decimals(one pound is 0.45359237 kilograms)
+        data['weight'] = round(data.weight * 0.45359237,2)
+        return data
 
+#loading
+def load(targetfile,data_to_load):
+    data_to_load.to_csv(targetfile)  
 
+#logging
+def log(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now() # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt","a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+#running ETL process
+log("ETL Job Started")
+log("Extract phase Started")
+extracted_data = extract()
+log("Extract phase Ended")
+extracted_data
+
+log("Transform phase Started")
+transformed_data = transform(extracted_data)
+log("Transform phase Ended")
+transformed_data 
+
+log("Load phase Started")
+load(targetfile,transformed_data)
+log("Load phase Ended")
+
+log("ETL Job Ended")
